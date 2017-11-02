9c19e01522222e36ac35397f87b71a088c201fa0
nat <nat.foster@gmail.com>
2017-11-02 10:01:43 -0600

    counter working with synchronous redis

--- app.py
+++ app.py
@@ -1,42 +1,25 @@
 from sanic import Sanic, response
-# import aioredis
-
+from redis import StrictRedis
 from config import REDIS_URL
 
 
 app = Sanic(__name__)
+redis_store = StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 
 @app.route('/')
 async def test(request):
-    async with request.app.redis_pool.get() as redis:
-        count = await redis.get('simple_count')
-        try:
-            count += 1
-        except SyntaxError:
-            count = 1
-        await redis.set('simple_count', count)
-
+    try:
+        count = int(redis_store.get('simple_count'))
+        count += 1
+    except TypeError:
+        count = 1
+    redis_store.set('simple_count', count)
     response_data = {
         'message': 'Hello, world!',
         'count': count
     }
     return response.json(response_data)
-
-
-@app.listener('before_server_start')
-async def before_server_start(app, loop):
-    # app.redis_pool = await aioredis.create_pool(
-    #     REDIS_URL,
-    #     minsize=5,
-    #     maxsize=10
-    # )
-
-
-@app.listener('after_server_stop')
-async def after_server_stop(app, loop):
-    app.redis_pool.close()
-    await app.redis_pool.wait_closed()
 
 
 if __name__ == '__main__':
